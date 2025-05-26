4a61b386ff74abb4bad0fb3eabade58860f6ae21
HsinyuG <xavier_gao@protonmail.com>
2025-05-26 18:52:45 +0200

    custom pylint rules

--- src/package_model.py
+++ src/package_model.py
@@ -9,7 +9,11 @@ import shutil
 model_dir = "release_model"
 os.makedirs(model_dir, exist_ok=True)
 shutil.copy(
-    "models/sentiment_classifier.joblib", f"{model_dir}/sentiment_classifier.joblib"
+    os.path.join("models", "sentiment_classifier.joblib"),
+    os.path.join(model_dir, "sentiment_classifier.joblib")
 )
-shutil.copy("metrics/metrics.json", f"{model_dir}/metadata.json")
+shutil.copy(
+    os.path.join("metrics", "metrics.json"),
+    os.path.join(model_dir, "metadata.json")
+)
 shutil.make_archive("model_release", "zip", model_dir)
